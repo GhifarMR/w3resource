895afc64b7baa44e5c430ee869ca786889bb0eb4
Ghifar Maulana Ristyono <tyrtwqptllpck@gmail.com>
2025-10-24 20:10:54 +0700

    First Commit

---
+++ Basic Part-1/HelloAndNamePrinter/src/helloandnameprinter/HelloAndNamePrinter.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package helloandnameprinter;
+
+/**
+ *
+ * @author 62852
+ */
+public class HelloAndNamePrinter {
+    public static void main(String args[]) {
+        System.out.println("Hello");
+        System.out.println("Ghifar");
+    }
+}
---
+++ Basic Part-1/SumOfTwoNumbers/src/sumoftwonumbers/SumOfTwoNumbers.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package sumoftwonumbers;
+
+/**
+ *
+ * @author 62852
+ */
+public class SumOfTwoNumbers {
+    public static void main(String args[]) {
+        int sum = 74 + 36;
+        System.out.println(sum);
+    }
+}
